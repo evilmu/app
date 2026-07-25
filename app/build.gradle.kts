44d640601d19ef1a18cb9469d01605419e6f69a1
bhagavad <marcosbsu@msn.com>
2026-07-25 04:12:29 -0300

    Atualiza versão para painel 3.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "br.gov.bomsucesso.threadsdownloader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "2.1.0"
+        versionCode = 30
+        versionName = "3.0.0"
     }
 
     buildFeatures {
