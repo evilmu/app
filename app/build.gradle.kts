397febeda147284d007f69c3deeb5a187ee06b06
bhagavad <marcosbsu@msn.com>
2026-07-25 20:33:47 -0300

    Release 3.3.4 with inline Threads-style action

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "br.gov.bomsucesso.threadsdownloader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 36
-        versionName = "3.3.3-relsposed"
+        versionCode = 37
+        versionName = "3.3.4-relsposed"
     }
 
     buildTypes {
